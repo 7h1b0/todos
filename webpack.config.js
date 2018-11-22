2aec42a2912164b89fa990bc6837f5094701201a
7h1b0 <7h1b0@users.noreply.github.com>
2018-11-22 22:01:35 +0100

    chore(webpack); move to a function

--- webpack.config.js
+++ webpack.config.js
@@ -5,87 +5,90 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
-const plugins = isProduction
-  ? [
-      new ManifestPlugin({
-        seed: {
-          short_name: 'Todos',
-          name: 'Todos',
-          background_color: '#eeeeee',
-          display: 'standalone',
-          theme_color: '#eeeeee',
-        },
-      }),
-      new SWPrecacheWebpackPlugin({
-        filename: 'service-worker.js',
-        dontCacheBustUrlsMatching: /\.\w{8}\./,
-        filename: 'service-worker.js',
-        minify: true,
-        staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
-      }),
-    ]
-  : [];
+module.exports = ({ prod = false }) => {
+  const plugins = prod
+    ? [
+        new ManifestPlugin({
+          seed: {
+            short_name: 'Todos',
+            name: 'Todos',
+            background_color: '#eeeeee',
+            display: 'standalone',
+            theme_color: '#eeeeee',
+          },
+        }),
+        new SWPrecacheWebpackPlugin({
+          filename: 'service-worker.js',
+          dontCacheBustUrlsMatching: /\.\w{8}\./,
+          filename: 'service-worker.js',
+          minify: true,
+          staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+        }),
+      ]
+    : [];
 
-module.exports = {
-  mode: isProduction ? 'production' : 'development',
-  entry: './src',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name]-[contenthash].js',
-    pathinfo: !isProduction,
-  },
-  devtool: isProduction ? 'none' : 'eval-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js/i,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
+  return {
+    mode: prod ? 'production' : 'development',
+    entry: './src',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name]-[contenthash].js',
+      pathinfo: !prod,
+    },
+    devtool: prod ? 'none' : 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js/i,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        minify: prod
+          ? {
+              removeComments: true,
+              collapseWhitespace: true,
+              removeRedundantAttributes: true,
+              useShortDoctype: true,
+              removeEmptyAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              keepClosingSlash: true,
+              minifyJS: true,
+              minifyCSS: true,
+              minifyURLs: true,
+            }
+          : undefined,
+      }),
+      new CleanWebpackPlugin('dist', { verbose: false }),
+      ...plugins,
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
-    }),
-    new CleanWebpackPlugin('dist', { verbose: false }),
-    ...plugins,
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'src'),
-    compress: true,
-    historyApiFallback: true,
-    noInfo: true,
-    port: 3000,
-  },
-  bail: true,
-  node: false,
-  stats: {
-    assets: true,
-    cached: false,
-    chunks: false,
-    children: false,
-    modules: false,
-    hash: false,
-    version: true,
-    timings: true,
-    warnings: true,
-    errors: true,
-    errorDetails: true,
-    builtAt: false,
-    entrypoints: false,
-  },
+    devServer: {
+      contentBase: path.join(__dirname, 'src'),
+      compress: true,
+      historyApiFallback: true,
+      noInfo: true,
+      port: 3000,
+    },
+    bail: true,
+    node: false,
+    stats: {
+      assets: true,
+      cached: false,
+      chunks: false,
+      children: false,
+      modules: false,
+      hash: false,
+      version: true,
+      timings: true,
+      warnings: true,
+      errors: true,
+      errorDetails: true,
+      builtAt: false,
+      entrypoints: false,
+    },
+  };
 };
