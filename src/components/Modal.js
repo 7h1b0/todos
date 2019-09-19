277bb5ac5d0d9f95ecdd71d0a19d31556c1d1f90
7h1b0 <7h1b0@users.noreply.github.com>
2019-09-19 23:41:08 +0200

    feat: add escape shortcut for modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,21 @@
 import { h } from 'preact';
+import { useEffect } from 'preact/hooks';
+
 import { useModal } from 'contexts/ModalContext';
 
 const Modal = ({ children }) => {
   const { open, closeModal } = useModal();
+
+  useEffect(() => {
+    window.addEventListener('keyup', event => {
+      if (event.code === 'Escape') {
+        closeModal();
+      }
+    });
+
+    return () => window.removeEventListener('keyup');
+  }, []);
+
   if (open) {
     return (
       <div class="overlay" onClick={closeModal}>
