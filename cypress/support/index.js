f7874c567e0a54d2de6615595bb9c80c7dac947a
7h1b0 <7h1b0@users.noreply.github.com>
2020-01-21 23:18:11 +0100

    fix: move to a controled input

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,5 @@
 Cypress.Commands.add('addTodo', (category, label) => {
   cy.get(`[aria-label="Add ${category} todo"]`).click();
   cy.get('input[name="add"]').type(label);
-  cy.wait(100);
   cy.contains('ADD TASK').click();
 });
