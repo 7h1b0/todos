5cf103f899bb6332c931795a193baddeb2889ef6
7h1b0 <7h1b0@users.noreply.github.com>
2020-07-17 23:19:08 +0200

    test(coverage): update collect coverage

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   errorOnDeprecated: true,
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
   testEnvironment: 'jest-environment-node',
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
 };
