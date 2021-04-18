b3d1247fce3eaebc2bd423d0bca077ca4fcb102b
7h1b0 <7h1b0@users.noreply.github.com>
2021-04-18 18:12:21 +0200

    chore: decrease SVG size, update CSS

--- src/components/Task.js
+++ src/components/Task.js
@@ -37,11 +37,11 @@ function Task(props) {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          alt="Remove"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
           aria-hidden="true"
         >
-          <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+          <path stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
       </button>
     </div>
