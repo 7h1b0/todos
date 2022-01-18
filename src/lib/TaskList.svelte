a7243aea4b5607c1af3ce457c17eda05a3af1577
7h1b0 <7h1b0@users.noreply.github.com>
2022-01-18 20:30:21 +0100

    refactor(ui): update spacing

--- src/lib/TaskList.svelte
+++ src/lib/TaskList.svelte
@@ -44,34 +44,36 @@
   on:dragleave={handleDragLeave}
   on:drop={handleDragDrop}
 >
-  <h2 id={categoryId}>{label}</h2>
-  {#if showForm}
-    <AddTask
-      {categoryId}
-      onClose={() => {
-        showForm = false;
-      }}
-    />
-  {:else}
-    <button
-      class="add"
-      type="button"
-      on:click={() => {
-        showForm = true;
-      }}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        aria-hidden="true"
+  <div class="header">
+    <h2 id={categoryId}>{label}</h2>
+    {#if showForm}
+      <AddTask
+        {categoryId}
+        onClose={() => {
+          showForm = false;
+        }}
+      />
+    {:else}
+      <button
+        class="add"
+        type="button"
+        on:click={() => {
+          showForm = true;
+        }}
       >
-        <path
-          d="M11 9V5H9v4H5v2h4v4h2v-4h4V9h-4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20z"
-        />
-      </svg>
-      Add {label} task
-    </button>
-  {/if}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          aria-hidden="true"
+        >
+          <path
+            d="M11 9V5H9v4H5v2h4v4h2v-4h4V9h-4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20z"
+          />
+        </svg>
+        Add {label} task
+      </button>
+    {/if}
+  </div>
 
   {#each sortedTasks as task}
     <Task {...task} />
