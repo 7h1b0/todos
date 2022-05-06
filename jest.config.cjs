d8f0533e9594788f94a4136b8e681c28340b8be0
7h1b0 <7h1b0@users.noreply.github.com>
2022-05-06 19:46:04 +0200

    chore(deps): upgrade dependencies

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
   testEnvironment: 'jest-environment-node',
   collectCoverageFrom: ['src/**/*.{js,jsx}'],
+  reporters: ['default', 'github-actions'],
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
