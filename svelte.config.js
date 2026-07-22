dd012b885e41a7d5aa9f6d495a9055df82d7e922
7h1b0 <7h1b0@users.noreply.github.com>
2026-07-22 19:01:08 +0200

    chore(compiler): set runes mode to true

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import { sveltePreprocess } from 'svelte-preprocess';
 export default {
   preprocess: sveltePreprocess(),
   compilerOptions: {
+    runes: true,
     cssHash: ({ hash, filename, css }) => `_${hash(filename ?? css)}`,
   },
 };
